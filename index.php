258738d7a952b7195540fd14a73dc12aa2f35bb8
Jack Skiles <jackskiles420@gmail.com>
2020-10-14 19:22:38 -0400

    some array practice

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
 $daysInWeek = 7;
 
 $name = 'Frank';
+$people = [
+    ['bill' => 'hello'],
+    ['bill' => 'hi']
+];
 
 echo $name;
 echo $daysInWeek . ' days in week';
+echo $people[0]['bill'];
+echo '<h1>' . "hello" . '</h1>';
 echo '<a href="http://www.google.com">Link</a>';
