fb5550269885f02e130716449a0559c29b6600cf
Jack Skiles <jackskiles420@gmail.com>
2020-11-02 13:47:00 -0500

    tested out posting with search

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ $pages = 10;
 
 echo '<h3>Searching for: ' . $searchTerm . '</h3>';
 echo '<p>You are on page ' . $page . '</p>';
+
+for ($i = 0; $i <= $pages; $i++){
+    echo '<a href="?search=' . $searchTerm . '$page=' . $i . '">' . $i . '</a> ';
+}
 // require_once 'functions/user.php';
 
 // echo fullName('Dale', 'Ernheart');
